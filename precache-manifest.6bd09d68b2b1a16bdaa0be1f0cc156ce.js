416bf1dbcea83eb579b3291236361815505c858b
Samir Sigh <samirsinghpat82@gmail.com>
2020-09-07 02:31:58 +0530

    Updates

---
+++ precache-manifest.6bd09d68b2b1a16bdaa0be1f0cc156ce.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad0abc5ad108723c6479252c401b0c4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7669b1139a9f32f4d6fe",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "6d03f0579050bd696d0e",
+    "url": "/static/css/main.d8cd27d9.chunk.css"
+  },
+  {
+    "revision": "7669b1139a9f32f4d6fe",
+    "url": "/static/js/2.07b1e26e.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/static/js/2.07b1e26e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d03f0579050bd696d0e",
+    "url": "/static/js/main.e7dd2d37.chunk.js"
+  },
+  {
+    "revision": "d18390a80186e6979c3c",
+    "url": "/static/js/runtime-main.4c96ffd2.js"
+  },
+  {
+    "revision": "bdab93b05643a6718fbc354f0e586420",
+    "url": "/static/media/Capture1.bdab93b0.PNG"
+  },
+  {
+    "revision": "db5666923e7f6610eb64c3fccd400fba",
+    "url": "/static/media/Capture2.db566692.PNG"
+  },
+  {
+    "revision": "4143195e1929673f560a53230d971c84",
+    "url": "/static/media/Capture3.4143195e.PNG"
+  },
+  {
+    "revision": "6f6081c60ff2b83de56cf50f2ee6237b",
+    "url": "/static/media/Capture4.6f6081c6.PNG"
+  },
+  {
+    "revision": "eccda8cda8769dfbe832ef64718779fc",
+    "url": "/static/media/Javascript.eccda8cd.png"
+  },
+  {
+    "revision": "a728aafbdfa918bb2c429d126ad19a46",
+    "url": "/static/media/bootstrap.a728aafb.png"
+  },
+  {
+    "revision": "0e8a418787fe7f3450492b89ef605cae",
+    "url": "/static/media/css3.0e8a4187.jpg"
+  },
+  {
+    "revision": "3645f9d4a29bad65733cb5ef1531dd62",
+    "url": "/static/media/git.3645f9d4.jpg"
+  },
+  {
+    "revision": "ee8e45f90ceb55fa72590ee1093d57f2",
+    "url": "/static/media/github-pages.ee8e45f9.jpg"
+  },
+  {
+    "revision": "6c06a196dd0a1b78bbd85af650923d69",
+    "url": "/static/media/html.6c06a196.png"
+  },
+  {
+    "revision": "e4fae5f3672d7a364097b22ec1a536d2",
+    "url": "/static/media/java.e4fae5f3.jpg"
+  },
+  {
+    "revision": "a581c87d736bf3833966cc6c348d1a64",
+    "url": "/static/media/material-ui.a581c87d.png"
+  },
+  {
+    "revision": "af0dad0aff317476578e26326f7e565a",
+    "url": "/static/media/my-sql.af0dad0a.png"
+  },
+  {
+    "revision": "7bc6dde9f496cf25e7b22c0506a64197",
+    "url": "/static/media/nodejs.7bc6dde9.png"
+  },
+  {
+    "revision": "ee209377c30e5be43c586155e4c189e7",
+    "url": "/static/media/pic2.ee209377.jpg"
+  },
+  {
+    "revision": "1e1d2d66469c22500e6ee365cfb79e00",
+    "url": "/static/media/pic3.1e1d2d66.jpg"
+  },
+  {
+    "revision": "9a6610f99470f9c7572e8e8846b813da",
+    "url": "/static/media/react-router.9a6610f9.jpg"
+  },
+  {
+    "revision": "0378caff00f03818cf32dbac7c1e83c0",
+    "url": "/static/media/react.0378caff.png"
+  },
+  {
+    "revision": "4912f6d800e1b75354e0b5d4794e1856",
+    "url": "/static/media/redux.4912f6d8.png"
+  },
+  {
+    "revision": "8e146a89c209afbed2500f2e74667302",
+    "url": "/static/media/samir.8e146a89.jpg"
+  },
+  {
+    "revision": "ea3b5829df8917ecfde66d2527919d59",
+    "url": "/static/media/typing.ea3b5829.jpg"
+  }
+]);
